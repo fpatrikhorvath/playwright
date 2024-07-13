ff3e304b40faaf58691aab40e5123246ccf72af3
Patrik Horvath <ferencpatrikhorvath@gmail.com>
2024-07-13 08:22:04 +0200

    TestRunner update

---
+++ src/main/java/com/playwright/framework/runner/TestRunner.java
@@ -0,0 +1,19 @@
+package com.playwright.framework.runner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
+
+@CucumberOptions(
+        features = {"classpath:features"},
+        glue = {"com.playwright.framework.stepdefs"},
+        tags = "not @bug",
+        plugin = "html:target/report.html"
+)
+public class TestRunner extends AbstractTestNGCucumberTests {
+    @Override
+    @DataProvider(parallel = false)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
+}
